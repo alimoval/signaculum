8118ef119826bf750d9205704faaf8d91e014463
Alik <nikovonet@gmail.com>
2018-01-11 02:02:23 +0200

    local routes, containers updated, year updated

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use(function (req, res, next) {
 app.use('/', products);
 app.use('/api', orders);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+});
